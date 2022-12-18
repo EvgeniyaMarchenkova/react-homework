ccfe094ac2881a30fa742f26fee154ca2d9141fa
Evgeniya Marchenkova <evgeniya_marchenkova@epam.com>
2022-12-18 15:24:33 +0100

    homework-7

--- src/store/moviesSlice.ts
+++ src/store/moviesSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from './store';
-import { Genre, MovieData } from '../model';
+import { MovieData } from '../model';
 
 interface MoviesState {
   selectedMovie: MovieData;
